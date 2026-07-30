d2050874f85bfba28d8227a2a00ed843ed1b1099
Christian Pacho <cdpacho@gmail.com>
2026-07-30 17:32:25 -0500

    refactor(db): remove default template controllers and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DbModule } from './db/db.module';
 
 @Module({
   imports: [DbModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
