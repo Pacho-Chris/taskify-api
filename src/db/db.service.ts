9c5538fe1f2ea98fa6c9ac3536f478f5d1058d62
Christian Pacho <cdpacho@gmail.com>
2026-07-30 19:06:26 -0500

    fix(db): resolve v1 type errors and implement defineRelations

--- src/db/db.service.ts
+++ src/db/db.service.ts
@@ -2,18 +2,18 @@ import { Injectable, OnModuleDestroy } from '@nestjs/common';
 import 'dotenv/config';
 import { drizzle, NodePgDatabase } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
-import * as schema from './schema';
+import { relations } from './relations';
 
 @Injectable()
 export class DbService implements OnModuleDestroy {
   private readonly pool: Pool;
-  public readonly db: NodePgDatabase<typeof schema>;
+  public readonly db: NodePgDatabase<typeof relations>;
 
   constructor() {
     this.pool = new Pool({
       connectionString: process.env.DATABASE_URL!,
     });
-    this.db = drizzle({ client: this.pool });
+    this.db = drizzle({ client: this.pool, relations });
   }
 
   async onModuleDestroy() {
