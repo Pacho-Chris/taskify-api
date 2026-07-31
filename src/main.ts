7f75e6e8a369d86aa5679a2d81b53c1a995429a7
Christian Pacho <cdpacho@gmail.com>
2026-07-31 11:45:30 -0500

    feat(config): configure initial Swagger settings

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Taskify API')
+    .setDescription(
+      'API para la gestión de tareas y usuarios con Nestjs Y Drizzle ORM',
+    )
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, documentFactory);
 
   app.useGlobalPipes(
     new ValidationPipe({
